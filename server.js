41b46eff37202cbde75f1f68493da7980e54b003
Aisha Ramiro <79142932+aisha-ramiro@users.noreply.github.com>
2025-05-29 21:51:40 -0300

    Criação de banco de dados mysql, requisição de api

--- server.js
+++ server.js
@@ -1,16 +1,57 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const server = express();
 const port = 3000;
 
-app.use(express.json());
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+}
 
-app.get('/saudacao', (req, res) => {
-    const data = {
-        mensagem: "API disponibilizada pelos alunos do 3º Semestre ADS Noite"
+server.use(express.json())
+
+server.get('/cidades', async (req, res) => {
+    let conexao;
+    try
+    {
+        conexao = await mysql.createConnection(dbConfig);
+        const [rows, fields] = await conexao.execute('SELECT * FROM CIDADE');
+
+        res.json({
+            mensagem: 'Cidades recuperadas com sucesso!',
+            data: rows
+        })
     }
-    res.json(data);
+    catch (error)
+    {
+        console.error('Erro ao buscar as cidades: ', error);
+    } 
 });
 
-app.listen(port, () => {
-    console.log("Acesse http://localhost:3000/saudacao");
+server.get('/empresas', async (req, res) => {
+    let conexao;
+    try
+    {
+        conexao = await mysql.createConnection(dbConfig);
+        const [rows, fields] = await conexao.execute('SELECT * FROM EMPRESA');
+
+        res.json({
+            mensagem: 'Empresas recuperadas com sucesso!',
+            data: rows
+        })
+    }
+    catch (error)
+    {
+        console.error('Erro ao buscar as empresas: ', error);
+    } 
+});
+
+server.listen(port, ()=>{
+    console.log('Servidor rodando em http://localhost:3000/cidades');
+    console.log('Servidor rodando em http://localhost:3000/empresas');
 })
